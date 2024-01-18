47ce76bcc421bd63eef5f07fe76677c71532c9b5
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-01-18 11:12:25 +0200

    Add files via upload

---
+++ 18.01.24/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MyClass myClass = new MyClass();
+            //myClass.Value1 = 5.8;
+            //myClass.Value2 = 4.2;
+
+            MyClass myClass1 = new MyClass(6.8, 3.2);
+            //myClass.Value1 = 6.8;
+            //myClass.Value2 = 3.2;
+            
+            Console.WriteLine($"{MyClass.count}");
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
